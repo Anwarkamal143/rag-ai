6c155d46c343345f6075c0caf248bc6617ff2afa
Anwarkamal143 <anwarkamal1434@gmail.com>
2026-09-12 15:24:34 +0500

    after branch protection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-    new change
+     after branch protection
       </div>
     </div>
   )
